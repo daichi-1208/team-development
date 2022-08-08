6dd74ef124c18ec5ef5587c2e158465d6dbc0e74
daichi.suzuki <daichi.suzuki@macbookpro.lan>
2022-08-08 23:48:14 +0900

    app.phpにライブラリ追記

--- backend/config/app.php
+++ backend/config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // OGP取得用ライブラリ
+        shweshi\OpenGraph\Providers\OpenGraphProvider::class,
+
     ],
 
     /*
