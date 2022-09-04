eec630ce6e550e2bd726167e93fb33ec47f43854
daichi.suzuki <daichi.suzuki@macbookpro.lan>
2022-09-05 00:45:02 +0900

    genreAPI修正

--- backend/app/Http/Controllers/Api/V1/GenreController.php
+++ backend/app/Http/Controllers/Api/V1/GenreController.php
@@ -4,20 +4,26 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Services\GenreService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class GenreController extends Controller
 {
     private $genreService;
 
+    private const SUCCESS_MASSAGE = 'Genre API Responce Success';
+
     public function __construct(GenreService $genreService)
     {
         $this->genreService = $genreService;
     }
 
-    public function fetchGenreLists(Request $request): JsonResponse
+    /**
+     * ジャンル一覧を返却
+     * @return JsonResponse
+     */
+    public function fetchGenreLists(): JsonResponse
     {
-        $result = $this->genreService->fetchGenreLists();
-        return response()->json($result);
+        return returnMessage(true, self::SUCCESS_MASSAGE, $this->genreService->fetchGenreLists());
     }
 }
