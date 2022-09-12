122938f20dba8cb8495a1107b1ec87170d75e8f2
daichi.suzuki <daichi.suzuki@macbookpro.lan>
2022-09-13 00:15:00 +0900

    wip

---
+++ backend/app/Services/CommentService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Comment;
+
+class CommentService
+{
+    public function fetchBookmarkComments()
+    {
+        // ブックマークに紐づいたコメント一覧を取得
+    }
+
+    public function showUserOnlyComments()
+    {
+        // ブックマークに紐づいた自分が投稿したコメントだけが見えるやつ(公開・非公開があるため)
+    }
+
+    public function newComment()
+    {
+        // 名前の通り
+    }
+
+    public function updateComment()
+    {
+        // 名前の通り
+    }
+
+    public function deleteComment()
+    {
+        // 名前の通り
+    }
+}
